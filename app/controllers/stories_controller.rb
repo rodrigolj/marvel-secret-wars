71b73075be1ca07076390f9530f4e94b746d1edf
Rodrigo Lima Jaroszewski <rodrigolj@hey.com>
2024-04-08 23:52:47 -0300

    refactor: cleaning the stories methods

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,9 +1,7 @@
 class StoriesController < ApplicationController
   include MarvelClient
   def show
-    story_request = MarvelClient::Request.new("stories", 12801).run
-    @story = story_request["data"]["results"].first
-    characters_request = MarvelClient::Request.new("stories", 12801, "characters").run
-    @characters = characters_request["data"]["results"]
+    @story = MarvelClient::Request.new("stories", 12801).run
+    @characters = MarvelClient::Request.new("stories", 12801, "characters").run
   end
 end
--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -1,10 +1,14 @@
 module StoriesHelper
-  def title(resource)
-    resource["title"]
+  def story_title
+    story_results["title"]
   end
 
-  def description(resource)
-    resource["description"]
+  def story_description
+    story_results["description"]
+  end
+
+  def characters_results
+    @characters["data"]["results"]
   end
 
   def character_thumbnail(resource)
@@ -15,5 +19,14 @@ module StoriesHelper
 
   def character_name(resource)
     resource["name"]
+  end
+
+  def attribution_text(resource)
+    resource["attributionText"]
+  end
+
+  private
+  def story_results
+    @story["data"]["results"].first
   end
 end
